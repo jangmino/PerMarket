813a8213e026f34bb80dbb439c6be791fc694c88
jangmino <jangmin.o@gmail.com>
2018-03-02 09:39:54 +0900

    휴일 작업

--- builder.py
+++ builder.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import csv
 import math
+import numpy as np
 from collections import OrderedDict
 
 
@@ -65,10 +66,10 @@ def AdjustSortedPAfterSampling(SortedP, u):
     if u in sortedHist: del sortedHist[u]
 
 
-def BuildUserSummary(fpath, skipFirstLine = True, logStep = 1000000):
-  P = {}
+def BuildUserInformation(fpath, categoryToIdx, skipFirstLine = True, logStep = 1000000):
   UserDic = {}
   numLines = 0
+  C = len(categoryToIdx)
   SortedP = {}
   with open(fpath, 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
@@ -79,28 +80,59 @@ def BuildUserSummary(fpath, skipFirstLine = True, logStep = 1000000):
       if len(row) != 3: raise RuntimeError("DealSummary's row should be size two, but :{}:".format(' '.join(row)))
       user, category, count = row[0], row[1], int(row[2].replace(',',''))
 
+      if category not in categoryToIdx:
+        continue
+      catIdx = categoryToIdx[category]
+
       if user not in UserDic:
-        UserDic[user] = 1
-      else:
-        UserDic[user] += 1
-      if category not in P:
-        P[category] = Hist(count, {user: count})
-      else:
-        P[category].vec[user] = count
-        P[category].total += count
+        UserDic[user] = np.zeros(C,)
+      UserDic[user][catIdx] += count
+  return UserDic
 
-    # Make OrderedDic
-    sys.stderr.write("\nsorting histograms per category...")
-    for c in list(P.keys()):
-      SortedP[c] = OrderedDict(sorted(P[c].vec.items(), key=lambda t:t[1]))
-      del P[c]
-    sys.stderr.write("done\n")
+    # # Make OrderedDic
+    # sys.stderr.write("\nsorting histograms per category...")
+    # for c in list(P.keys()):
+    #   SortedP[c] = OrderedDict(sorted(P[c].vec.items(), key=lambda t:t[1]))
+    #   del P[c]
+    # sys.stderr.write("done\n")
+  #
+  # return SortedP, UserDic, numLines
 
-  return SortedP, UserDic, numLines
+def BuildCategoryToUsers(fpath, categoryToIdx, logStep = 1000000):
+  numLines = 0
+  C = len(categoryToIdx)
+  SortedP = {}
+  userToIdx = {}
+  categoryDic = {}
+  categoryMatrix = None
+  for c in range(C): categoryDic[c] = {}
+  sys.stderr.write("...now reading.\n")
+  with open(fpath, 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in reader:
+      numLines += 1
+      if numLines == 1:
+        dimUser, dimCategory = int(row[0]), int(row[1])
+        categoryMatrix = np.zeros((dimUser, dimCategory))
+        continue
+      if numLines % logStep == 0: sys.stderr.write("......{} lines are read.\n".format(numLines))
+      if len(row) != C+1: raise RuntimeError("user summary's row should be size {}, but :{}:".format(C+1, ' '.join(row)))
+      userIdx = int(row[0])
+      vec = np.asarray([float(v) for v in row[1:]])
+      categoryMatrix[userIdx,:] = vec/vec.sum()
+
+    sys.stderr.write("...now sorting.\n")
+    for c in range(C):
+      ii = np.argsort(categoryMatrix[:,c])
+      categoryDic[c] = OrderedDict(zip(ii, categoryMatrix[ii,c].tolist()))
+    sys.stderr.write("...done\n")
+    return categoryDic, userToIdx
+
 
 
 def BuildDealSummary(fpath, skipFirstLine = True):
-  D = {}
+  dealToCategory = {}
+  categoryToIdx = {}
   numLines = 0
   with open(fpath, 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
@@ -109,10 +141,12 @@ def BuildDealSummary(fpath, skipFirstLine = True):
       if skipFirstLine == True and numLines == 1: continue
       if len(row) != 2: raise RuntimeError("DealSummary's row should be size two, but :{}:".format(' '.join(row)))
       deal, category = row[0], row[1]
-      if deal not in D:
-        D[deal] = category
+      if deal not in dealToCategory:
+        dealToCategory[deal] = category
+      if category not in categoryToIdx:
+        categoryToIdx[category] = len(categoryToIdx)
 
-  return D
+  return dealToCategory, categoryToIdx
 
 def doLoop(O, P, DealToCategory, numUsers, numDeals):
   recSet = {}
@@ -144,6 +178,21 @@ def doLoop(O, P, DealToCategory, numUsers, numDeals):
 
   return recSet, notAssigend
 
+def WriteUserSummary(fpath, userDic, dimCategory):
+  '''
+
+  :param fpath:
+  :param userDic: user -> histograms of category ( order counts )
+  :return:
+  '''
+
+  n = 0
+  with open(fpath, 'w', encoding='utf-8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow([len(userDic), dimCategory])
+    for user, counts in userDic.items():
+      writer.writerow([n] + [count for count in counts])
+      n += 1
 
 def WriteResult(fpath, recResult):
   with open(fpath, 'w', encoding='utf-8', newline='') as f:
@@ -155,37 +204,44 @@ def WriteResult(fpath, recResult):
 ## main 설계
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("-u", "--user", help="the file path of user-order summary")
+  parser.add_argument("-i", "--input", help="the file path of user-order information [or user summary]")
   parser.add_argument("-d", "--deal", help="the file path of deal-category information")
-  parser.add_argument("-o", "--output", help="the file path of output user-deal recommendation")
+  parser.add_argument("-o", "--output", help="the file path of output")
+  parser.add_argument("-s", "--summary", action="store_true", help="build user summary file")
+  parser.add_argument("-m", "--match", action="store_true", help="match user to deal")
   args = parser.parse_args()
 
-  if args.user == None or args.deal == None or args.output == None:
+  if args.input == None or args.deal == None or args.output == None:
     parser.print_usage()
     sys.exit(1)
 
-  D = BuildDealSummary(args.deal)
-  sortedP, UserDic, numLines = BuildUserSummary(args.user)
+  dealToCategory, categoryToIdx = BuildDealSummary(args.deal)
+  if args.summary:
+    UserDic = BuildUserInformation(args.input, categoryToIdx)
+    WriteUserSummary(args.output, UserDic, len(categoryToIdx))
+    sys.exit(0)
+  elif args.match:
+    categoryDic, userToIdx = BuildCategoryToUsers(args.input, categoryToIdx)
 
-  numDeals, numUsers = len(D), len(UserDic)
-  print("#users: {}, #deals: {}, #lines: {}".format(numUsers, numDeals, numLines))
-
-  # prepare
-  O = Hist(numUsers, {})
-  for deal in D.keys():
-    O.vec[deal] = math.ceil(numUsers / numDeals)
-
-  assigned, notAssgigned = doLoop(O, sortedP, D, numUsers, numDeals)
-
-  # verify
-  dealAssigned = {}
-  numUserAssigned = 0
-  for user, deal in assigned.items():
-    if deal not in dealAssigned: dealAssigned[deal] = 1
-    else: dealAssigned[deal] += 1
-    numUserAssigned += 1
-  # for deal, cnt in dealAssigned.items():
-  #   sys.stderr.write("{}:{}\n".format(deal, cnt))
-  sys.stderr.write("\n{} users are assgined over {}.\n".format(numUserAssigned, numUsers))
-
-  WriteResult(args.output, assigned)
+  # numDeals, numUsers = len(D), len(UserDic)
+  # print("#users: {}, #deals: {}, #lines: {}".format(numUsers, numDeals, numLines))
+  #
+  # # prepare
+  # O = Hist(numUsers, {})
+  # for deal in D.keys():
+  #   O.vec[deal] = math.ceil(numUsers / numDeals)
+  #
+  # assigned, notAssgigned = doLoop(O, sortedP, D, numUsers, numDeals)
+  #
+  # # verify
+  # dealAssigned = {}
+  # numUserAssigned = 0
+  # for user, deal in assigned.items():
+  #   if deal not in dealAssigned: dealAssigned[deal] = 1
+  #   else: dealAssigned[deal] += 1
+  #   numUserAssigned += 1
+  # # for deal, cnt in dealAssigned.items():
+  # #   sys.stderr.write("{}:{}\n".format(deal, cnt))
+  # sys.stderr.write("\n{} users are assgined over {}.\n".format(numUserAssigned, numUsers))
+  #
+  # WriteResult(args.output, assigned)
--- match_by_distribution.py
+++ match_by_distribution.py
@@ -5,6 +5,8 @@ import csv
 import math
 from collections import OrderedDict
 import numpy as np
+from scipy.special import digamma
+from scipy.special import gammaln
 
 class DirMult:
   def __init__(self, dim):
@@ -17,8 +19,10 @@ class Hist:
     self.vec = vec_
 
 def BuildDealSummary(fpath, skipFirstLine = True):
-  D = {}
+  dealDic = {}
   numLines = 0
+  dealToIdx = {}
+  categoryToIdx = {}
   with open(fpath, 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -26,37 +30,80 @@ def BuildDealSummary(fpath, skipFirstLine = True):
       if skipFirstLine == True and numLines == 1: continue
       if len(row) != 2: raise RuntimeError("DealSummary's row should be size two, but :{}:".format(' '.join(row)))
       deal, category = row[0], row[1]
-      if deal not in D:
-        D[deal] = category
+      if deal not in dealDic:
+        dealDic[deal] = category
+      if deal not in dealToIdx:
+        dealToIdx[deal] = len(dealToIdx)
+      if category not in categoryToIdx:
+        categoryToIdx[category] = len(categoryToIdx)
 
-  return D
+  return dealDic, dealToIdx, categoryToIdx
 
-def BuildUserSummary(fpath, categoryDic, skipFirstLine = True, logStep = 1000000):
-  P = {}
+def BuildUserSummary(fpath, categoryToIdx, skipFirstLine = True, logStep = 1000000):
   UserDic = {}
   numLines = 0
-  SortedP = {}
-  C = 10
-  CategoryToIndex = {}
-  prior = np.zeros(C,)
+  C = len(categoryToIdx)
+  total = np.zeros(C,)
+
+  userToIdx = {}
 
   with open(fpath, 'r', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
       numLines += 1
+      #if numLines == 10000: break
       if skipFirstLine == True and numLines == 1: continue
       if numLines % logStep == 0: sys.stderr.write("......{} lines are read.\n".format(numLines))
       if len(row) != 3: raise RuntimeError("DealSummary's row should be size two, but :{}:".format(' '.join(row)))
       user, category, count = row[0], row[1], int(row[2].replace(',',''))
 
+      if category not in categoryToIdx:
+        continue
+      catIdx = categoryToIdx[category]
       if user not in UserDic:
         UserDic[user] = np.zeros(C,)
-      idx = CategoryToIndex[category]
-      UserDic[user][idx] += 1.0
-      prior[idx] += 1.0
+      UserDic[user][catIdx] += count
 
-  return SortedP, UserDic, numLines
+      if user not in userToIdx:
+        userToIdx[user] = len(userToIdx)
 
+      total[catIdx] += count
+
+  return UserDic, total, userToIdx, numLines
+
+def BuildCategoryRankedUsers(UserDic, categoryToIdx, userToIdx):
+  categoryDic = {}
+  for c in range(len(categoryToIdx)): categoryDic[c] = []
+  for user, vec in UserDic.items():
+    for ii in np.argsort(vec):
+      categoryDic[ii].append((userToIdx[user], vec[ii]))
+  sys.stderr.write("Category Dictionary was prepared...\n")
+
+  for c in range(len(categoryToIdx)):
+    categoryDic[c] = OrderedDict(sorted(categoryDic[c], key=lambda t:-1*t[1]))
+  sys.stderr.write("Category Dictionary was sorted...\n")
+
+  return categoryDic
+
+
+def argmaxByDirchletRatio(p, q, alpha=1.0):
+  ## http://bariskurt.com/kullback-leibler-divergence-between-two-dirichlet-and-beta-distributions/
+
+  x = p + alpha
+  y = q + alpha
+
+  score = gammaln(y) - gammaln(x) +  (x - y) * (digamma(x) - digamma(x.sum()))
+  score.argmax()
+
+def doLoop(dealDic, total, userDic, categoryToIdx, numUsers, numDeals):
+  bulkSize = max(1, numUsers//100)
+  n = 0
+  while numUsers > 0 and numDeals > 0 and len(userDic) > 0 and n < numUsers:
+    score = argmaxByDirchletRatio(total, total)
+    n += 1
+    if n % bulkSize == 0: sys.stderr.write("{:.2f}% is processed.\n".format(100*n/numUsers))
+
+  sys.stderr.write("\n")
 
 ## main 설계
 if __name__ == "__main__":
@@ -70,8 +117,16 @@ if __name__ == "__main__":
     parser.print_usage()
     sys.exit(1)
 
-  # D = BuildDealSummary(args.deal)
-  # sortedP, UserDic, numLines = BuildUserSummary(args.user)
+  dealDic, dealToIdx, categoryToIdx = BuildDealSummary(args.deal)
+  userDic, total, userToIdx, numLines = BuildUserSummary(args.user, categoryToIdx)
+  categoryDic = BuildCategoryRankedUsers(userDic, categoryToIdx, userToIdx)
+
+  while True:
+    x=1
+
+  doLoop(dealDic, total, userDic, categoryToIdx, len(userDic), len(dealDic))
+
+
   #
   # numDeals, numUsers = len(D), len(UserDic)
   # print("#users: {}, #deals: {}, #lines: {}".format(numUsers, numDeals, numLines))
